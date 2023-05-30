9011076ef2752d00e69afdeedb5d30ef31ddc376
Marina Gabrielly <marina.gabri3lly@gmail.com>
2023-05-29 23:07:15 -0300

    finalização do projeto

--- js/main.js
+++ js/main.js
@@ -1,37 +1,39 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom (seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento && elemento.localName === 'audio') {
+        elemento.play();
+    }
+    else {
+        console.log('Elemento ou seletor não encontrado');
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla'); //recebe a lista com todas as teclas do Alura Fone
 
-
 for (let contador = 0; contador < listaDeTeclas.length; contador++) {
 
     const tecla = listaDeTeclas[contador];
-    const instrumento = tecla.classList[1]; //acessa a lista    
-    const idAudio = `#som_${instrumento}`
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;
 
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
 
+     //====== acessibilidade (navegação pelo teclado)
 
-    //====== acessibilidade (navegação pelo teclado)
+    tecla.onkeydown = function (evento) {
 
-    const listaDeTecla = document.querySelectorAll('input[type=button]');
-
-    for (let indice = 0; indice < listaDeTecla.length; indice++) {
-        const tecla = listaDeTecla[indice];
-
-        tecla.onkeydown = function (evento) { 
-
-        if (evento.code === "Enter" || evento.code === "Space") {
+        if (evento.code === 'Space' || evento.code === 'Enter') {
             tecla.classList.add('ativa');
-        } // se evento.code (o código do evento onkeydown) for igual a "Enter" ou for igual a "Space", adicione a classe ativa no elemento tecla,
         }
 
-        tecla.onkeyup = function () {
-            tecla.classList.remove('ativa'); //remove a classe logo após deixamos de pressioná-la
-        }
     }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+
 }
